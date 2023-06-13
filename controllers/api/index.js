1c8bde1b7323e006ad41f3e43e85318b8139992c
unknown <loltony79@gmail.com>
2023-06-13 13:59:29 -0400

    commit

---
+++ controllers/api/index.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const userRoutes = require("./userRoutes.js");
+const dishRoutes = require("./userRoutes.js");
+const ingredientRoutes = require("./ingredientRoutes.js");
+
+module.exports = router;
