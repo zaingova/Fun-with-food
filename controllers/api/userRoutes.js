341e420a0291e9f9a32be8e2395138d77cac2375
zaingova <zain.gova.school@gmail.com>
2023-06-14 14:54:12 -0400

    changed views.handlebars to have the class 'form-submit' on a form instead of a div

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 // Import router
 const router = require("express").Router();
 // Import user model
-const { User } = require("../../models/User");
+const User = require("../../models/User");
 
 // User login
 router.post("/login", async (req, res) => {
@@ -9,29 +9,26 @@ router.post("/login", async (req, res) => {
     // Check for email
     const userData = await User.findOne({ where: { email: req.body.email } });
     if (!userData) {
-      res
-        .status(400)
-        .json({
-          message: "Fail to login, wrong email or password please try again",
-        });
+      res.status(400).json({
+        message: "Fail to login, wrong email or password please try again",
+      });
       return;
     }
     // Check for password
     const validPassword = await userData.checkPassword(req.body.password);
     if (!validPassword) {
-      res
-        .status(400)
-        .json({
-          message: "Fail to login, wrong email or password, please try again!",
-        });
+      res.status(400).json({
+        message: "Fail to login, wrong email or password, please try again!",
+      });
       return;
     }
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      res.json({ user: userData, message: "You are now looged in!" });
+      res.json({ user: userData, message: "You are now logged in!" });
     });
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
