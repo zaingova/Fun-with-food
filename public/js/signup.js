f5da8e5824b5b0f2d26a2ec9b8745b5b07a8a62b
unknown <loltony79@gmail.com>
2023-06-16 10:03:27 -0400

    commit

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,32 +1,32 @@
-const name = document.getElementById("#name-signup").value.trim();
-const email = document.getElementById("#email-signup").value.trim();
-const password = document.getElementById("#password-signup").value.trim();
-const secondPassword = document.getElementById("secondPassword").value.trim();
+const signupFormHandler = async (event) => {
 
-const signup = async (event) => {
   event.preventDefault();
 
-  // inputs entry error function
-  // inputs entry correct function
-  // validate my inputs
-  const valideInput = () => {
-    const nameValue = name.trim();
-    const emailValue = email.trim();
-    const passwordValue = password.value.trim();
-    const secondPasswordValue = secondPassword.value.trim();
-  };
-  if (name && email && password) {
-    const response = await fetch("/api/user", {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
+  const username = document.querySelector('#username-signup').value.trim();
 
+  const email = document.querySelector('#email-signup').value.trim();
+
+  const password = document.querySelector('#password-signup').value.trim();
+
+  const validpassword = document.querySelector('#password-verify').value.trim();
+
+  if (username && email && password && validpassword && password === validpassword) {
+
+    const response = await fetch('/api/users', {
+
+      method: 'POST',
+
+      body: JSON.stringify({ username, email, password }),
+
+      headers: { 'Content-Type': 'application/json' },
+
+    });
     if (response.ok) {
-      document.location.replace("/");
+      
+      document.location.replace('/');
+      
     } else {
-      alert("signup failed.Try again");
+      alert('Failed to sign up.');
     }
   }
 };
-document.querySelector(".signup-form").addEventListener("submit", signup);
