7cee24592ad3d108ce629bed05e6636200d3104c
zaingova <zain.gova.school@gmail.com>
2023-06-17 16:59:47 -0400

    message

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,48 +1,54 @@
-// Import router
+// import router
 const router = require("express").Router();
-// Import user model
+
+// import user model
 const User = require("../../models/User");
 const Dish = require("../../models/Dish");
 
-// User login
-router.post("/signup", async (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  })
-    .then((userData) => {
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
+// import login authorization helper
+const withAuth = require('../../utils/auth');
 
-        res.json({ user: userData, message: "You are now logged in!" });
-      });
+// signup route
+router.post("/signup", async (req, res) => {
+  try {
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json({ user: userData, message: "You are now logged in!" });
     });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
 });
 
+// login route
 router.post("/login", async (req, res) => {
   try {
     // Check for email
     const userData = await User.findOne({ where: { email: req.body.email } });
+
+    // if no user with that email exists...
     if (!userData) {
-
       res.status(400).json({ message: "Fail to login, wrong email or password please try again" });
-
       return;
     }
+
     // Check for password
     const validPassword = await userData.checkPassword(req.body.password);
+
+    // if that password doesn't exist in the database...
     if (!validPassword) {
-
       res.status(400).json({ message: "Fail to login, wrong email or password, please try again!" });
-
       return;
     }
+
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -54,8 +60,11 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.post("/logout", (req, res) => {
+// logout route (withAuth isn't requireed for this but its there just in case something else goes wrong)
+router.post("/logout", withAuth, (req, res) => {
+  // if we are currently logged in... (its not actually possible to get to this route while logged out unless the url is entered manually)
   if (req.session.logged_in) {
+    // destroy the session
     req.session.destroy(() => {
       res.status(204).end();
     });
--- controllers/api/userdishRoutes.js
+++ controllers/api/userdishRoutes.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const User_Dish = require("../../models/User_Dish");
 const withAuth = require('../../utils/auth');
 
+// route for saving a dish to the database
 router.post('/', withAuth, async (req, res) => {
   try {
+    // adds a dish that has the same user_id
     const addedDish = await User_Dish.create({
       user_id: req.session.user_id,
       dish_id: req.body.dishData,
     });
-
-    //console.log(addedDish);
 
     res.status(200).json(addedDish);
   } catch (err) {
